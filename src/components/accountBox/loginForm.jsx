1eba3ae9a76e5f3146b5388448e87c39ed819310
precious28 <31155317+precious28@users.noreply.github.com>
2021-08-27 01:19:44 +0200

    commit

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -1,98 +1,33 @@
-import { Field, useFormik } from "formik";
-import React, { useContext, useState } from "react";
-import { Marginer } from "../marginer";
+import React, { useContext } from "react";
 import {
   BoldLink,
   BoxContainer,
-  FieldContainer,
-  FieldError,
   FormContainer,
-  FormError,
   Input,
   MutedLink,
   SubmitButton,
 } from "./common";
-import { AccountContext } from "./context";
-import * as yup from "yup";
-import axios from "axios";
-
-const validationSchema = yup.object({
-  email: yup.string().required(),
-  password: yup.string().required(),
-});
+import { Marginer } from "../marginer";
+import { AccountContext } from "./accountContext";
 
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
-  const [error, setError] = useState(null);
-
-  const onSubmit = async (values) => {
-    setError(null);
-    const response = await axios
-      .post("http://localhost:5000/api/v1/login", values)
-      .catch((err) => {
-        if (err && err.response) setError(err.response.data.message);
-      });
-
-    if (response) {
-      alert("Welcome back in. Authenticating...");
-    }
-  };
-
-  const formik = useFormik({
-    initialValues: { email: "", password: "" },
-    validateOnBlur: true,
-    onSubmit,
-    validationSchema: validationSchema,
-  });
 
   return (
     <BoxContainer>
-      <FormError>{error ? error : ""}</FormError>
-      <FormContainer onSubmit={formik.handleSubmit}>
-        <FieldContainer>
-          <Input
-            name="email"
-            placeholder="Email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {
-            <FieldError>
-              {formik.touched.email && formik.errors.email
-                ? formik.errors.email
-                : ""}
-            </FieldError>
-          }
-        </FieldContainer>
-        <FieldContainer>
-          <Input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          {
-            <FieldError>
-              {formik.touched.password && formik.errors.password
-                ? formik.errors.password
-                : ""}
-            </FieldError>
-          }
-        </FieldContainer>
-        <MutedLink href="#">Forgot Password?</MutedLink>
-        <Marginer direction="vertical" margin="1em" />
-        <SubmitButton type="submit" disabled={!formik.isValid}>
-          Login
-        </SubmitButton>
+      <FormContainer>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
       </FormContainer>
-      <Marginer direction="vertical" margin={5} />
+      <Marginer direction="vertical" margin={10} />
+      <MutedLink href="#">Forget your password?</MutedLink>
+      <Marginer direction="vertical" margin="1.6em" />
+      <SubmitButton type="submit">Signin</SubmitButton>
+      <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
-        Dont have an Account?
+        Don't have an accoun?{" "}
         <BoldLink href="#" onClick={switchToSignup}>
-          sign up
+          Signup
         </BoldLink>
       </MutedLink>
     </BoxContainer>
